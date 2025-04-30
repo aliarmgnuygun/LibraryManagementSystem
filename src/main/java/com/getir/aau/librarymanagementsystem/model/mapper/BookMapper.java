e09c13bb547cb9783cfe5ad0b8f5016a01c101f0
aliarmaganuygun <aliarmaganuygun@gmail.com>
2025-05-01 00:20:22 +0300

    Added BookMapper using MapStruct for BookResponseDto and BookRequestDto

---
+++ src/main/java/com/getir/aau/librarymanagementsystem/model/mapper/BookMapper.java
@@ -0,0 +1,24 @@
+package com.getir.aau.librarymanagementsystem.model.mapper;
+
+import com.getir.aau.librarymanagementsystem.model.dto.BookRequestDto;
+import com.getir.aau.librarymanagementsystem.model.dto.BookResponseDto;
+import com.getir.aau.librarymanagementsystem.model.entity.Author;
+import com.getir.aau.librarymanagementsystem.model.entity.Book;
+import com.getir.aau.librarymanagementsystem.model.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface  BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "available", expression = "java(dto.numberOfCopies() > 0)")
+    Book toEntity(BookRequestDto dto, Author author, Category category);
+
+    @Mapping(source = "author.name", target = "authorName")
+    @Mapping(source = "category.name", target = "categoryName")
+    BookResponseDto toDto(Book book);
+}
