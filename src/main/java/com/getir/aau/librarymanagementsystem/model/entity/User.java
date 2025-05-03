84df88347b1de6db1e7051cc32efef42feef5940
aliarmaganuygun <aliarmaganuygun@gmail.com>
2025-05-03 19:19:34 +0300

    User entity created

---
+++ src/main/java/com/getir/aau/librarymanagementsystem/model/entity/User.java
@@ -0,0 +1,43 @@
+package com.getir.aau.librarymanagementsystem.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName", length = 100,nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName",length = 100,nullable = false)
+    private String lastName;
+
+    @Column(name = "username", length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "email", length = 100, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "phoneNumber", length = 15, nullable = false)
+    private String phoneNumber;
+
+}
